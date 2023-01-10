3879b1913a7a2dcd1f4b9178346c23bacedde98f
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-01-10 19:55:13 +0000

    hashtag-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const accountRoutes = require('./post-routes');
 const postRoutes = require('./post-routes');
-const hashtagRoutes = require('./hashtag-routes');
+
 
 router.use('/account', accountRoutes);
 router.use('/post', postRoutes);
