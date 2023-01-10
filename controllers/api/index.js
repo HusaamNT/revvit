6c55d9a470e5dc62eef8aff2b98dd571782fee2f
Husaam NT <husnaeem@gmail.com>
2023-01-10 11:31:30 +0000

    added index routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const accountRoutes = require('./account-routes');
+const postRoutes = require('./post-routes');
+const hashtagRoutes = require('./hashtag-routes');
+
+router.use('/account', accountRoutes);
+router.use('/post', postRoutes);
+router.use('/hashtag', hashtagRoutes);
+
+module.exports = router;
