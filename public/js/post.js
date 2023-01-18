ed487f76579df5df54f6d9eeb78122bf4f3cf603
Husaam NT <husnaeem@gmail.com>
2023-01-18 21:30:10 +0000

    added find by id route and begun append post fetch

--- public/js/post.js
+++ public/js/post.js
@@ -1,1 +1,13 @@
+const viewPosts = () =>
+fetch('api/posts/',{
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+console.log(viewPosts)
+console.log(viewPosts.Bio)
+console.log(viewPosts.createdAt)
+const accountId = viewPosts.account_id
 
+const 
